488497db9bb3485dd35126ca8b6886ac31d3bfa9
murilloarauj <murilloaraujo055@gmail.com>
2026-03-12 21:56:49 -0300

    first commit

---
+++ src/main/java/com/facens/app_biblioteca_api/AppBibliotecaApiApplication.java
@@ -0,0 +1,13 @@
+package com.facens.app_biblioteca_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBibliotecaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBibliotecaApiApplication.class, args);
+	}
+
+}
